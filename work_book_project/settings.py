b440686036af4e2cf242e41ba08fafb3f8070ea7
Nour Saleh Rabee <rabee.nour123@gmail.com>
2024-08-20 17:42:37 +0300

    modify database settings

--- work_book_project/settings.py
+++ work_book_project/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'work_book_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'workbook_db',
+        'USER': 'root',
+        'PASSWORD': 'root123!',
+        'HOST': '127.0.0.1',  # or the hostname where your MySQL server is running
+        'PORT': '3306',  # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
